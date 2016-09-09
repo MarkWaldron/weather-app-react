c01701de472b0e1d9ae976cf58a5c7de07637d68
Mark Waldron <markewaldron@gmail.com>
2016-09-09 17:12:10 -0400

    Able to update view and plot data

--- src/App.js
+++ src/App.js
@@ -2,24 +2,27 @@ import React, { Component } from 'react';
 import './App.css';
 import xhr from 'xhr';
 
+import Plot from './Plot.js';
+
 class App extends Component {
 
-//API Key 6bac7e4a2329bd62bd8399fa395cd1ff
-  
   state = {
     location: '',
-    data: {}
+    data: {},
+    dates: [],
+    temps: [],
+    selected: {
+      date: '',
+      temp: null
+    }
   };
-  
   fetchData = (evt) => {
-
     evt.preventDefault();
     var location = encodeURIComponent(this.state.location);
-
-    var urlPrefix = 'http://api.openweathermap.org/data/2.5/forcast?q=';
+    var urlPrefix = 'http://api.openweathermap.org/data/2.5/forecast?q=';
     var urlSuffix = '&APPID=6bac7e4a2329bd62bd8399fa395cd1ff&units=imperial';
-    var url = urlPrefix + urlSuffix;
-    
+    var url = urlPrefix + location + urlSuffix;
+     
     var self = this;
     
     xhr({
@@ -36,7 +39,9 @@ class App extends Component {
       }
 
       self.setState({
-        data: body
+        data: body,
+        dates: dates,
+        temps: temps
       });
     });
   };
@@ -45,6 +50,17 @@ class App extends Component {
     this.setState({
       location: evt.target.value
     });
+  };
+
+  onPlotClick = (data) => {
+    if (data.points) {
+      this.setState({
+        selected: {
+          date: data.points[0].x,
+          temp: data.points[0].y
+        }
+      });
+    }
   };
   
   render() {
@@ -58,17 +74,32 @@ class App extends Component {
         <form onSubmit={this.fetchData}>
           <label>I want to know the weather for
             <input
-              placeholder={" City, Country "}
+              placeholder={"City, Country"}
               type="text"
               value={this.state.location}
               onChange={this.changeLocation}
             />
           </label>
         </form>
-        <p className="temp-wrapper">
-          <span className="temp">{ currentTemp }</span>
-          <span className="temp-symbol"> °F</span>
-        </p>
+        {/*
+          Render the current temperature and the forecast if we have data
+          otherwise return null
+        */}
+        {(this.state.data.list) ? (
+          <div className="wrapper">
+            <p className="temp-wrapper">
+              <span className="temp">{ currentTemp }</span>
+              <span className="temp-symbol"> °F</span>
+            </p>
+            <h2>Forecast</h2>
+            <Plot
+              xData={this.state.dates}
+              yData={this.state.temps}
+              onPlotClick={this.onPlotClick}
+              type="scatter"
+            />
+          </div>
+        ) : null}
       </div>
     );
   }
